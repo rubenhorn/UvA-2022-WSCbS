7521b278d9a3e3048c718ac5889f33b4b32ed785
Ruben Horn <horn.ruben@yahoo.de>
2022-04-06 15:27:36 +0200

    Test for authorization

--- assignment_1/test_server.py
+++ assignment_1/test_server.py
@@ -1,7 +1,8 @@
 import pytest
 
 test_url = 'https://www.example.org'
-test_auth_header = 'alice'
+test_auth_header_a = 'alice'
+test_auth_header_b = 'bob'
 test_key_exists = None
 test_key_not_exists = 'foo'
 
@@ -13,7 +14,7 @@ class TestBase:
         app.config['DEFAULT_USER'] = None
         app.config['EPHEMERAL'] = True # Do not use persistent storage for testing
         client = app.test_client()
-        response = client.post('/', json={'url': test_url}, headers={'Authorization': test_auth_header})
+        response = client.post('/', json={'url': test_url}, headers={'Authorization': test_auth_header_a})
         test_key_exists = response.json['data']['id']
         yield client
 
@@ -35,36 +36,46 @@ class TestInterface(TestBase):
         assert response.headers['Location'] == test_url
 
     def test_update_non_existing_key_should_fail(self, client):
-        response = client.put(f'/{test_key_not_exists}', json={'url': test_url}, headers={'Authorization': test_auth_header})
+        response = client.put(f'/{test_key_not_exists}', json={'url': test_url}, headers={'Authorization': test_auth_header_a})
         assert response.status_code == 404
 
     def test_update_existing_key_invalid_url_should_fail(self, client):
-        response = client.put(f'/{test_key_exists}', json={'url': 'foo'}, headers={'Authorization': test_auth_header})
+        response = client.put(f'/{test_key_exists}', json={'url': 'foo'}, headers={'Authorization': test_auth_header_a})
         assert response.status_code == 400
         assert response.json['status'] == 'error'
         assert response.json['data'] == { 'message': 'Invalid URL' }
 
     def test_update_existing_key_valid_url_should_succeed(self, client):
         new_url = 'https://www.google.com'
-        response = client.put(f'/{test_key_exists}', json={'url': new_url}, headers={'Authorization': test_auth_header})
+        response = client.put(f'/{test_key_exists}', json={'url': new_url}, headers={'Authorization': test_auth_header_a})
         assert response.status_code == 200
         response = client.get(f'/{test_key_exists}')
         assert response.status_code == 301
         assert response.headers['Location'] == new_url
 
     def test_delete_non_existing_key_should_fail(self, client):
-        response = client.delete(f'/{test_key_not_exists}', headers={'Authorization': test_auth_header})
+        response = client.delete(f'/{test_key_not_exists}', headers={'Authorization': test_auth_header_a})
         assert response.status_code == 404
 
     def test_delete_existing_key_should_succeed(self, client):
         global test_key_exists
-        response = client.delete(f'/{test_key_exists}', headers={'Authorization': test_auth_header})
+        response = client.delete(f'/{test_key_exists}', headers={'Authorization': test_auth_header_a})
         assert response.status_code == 204
-        response = client.post('/', json={'url': test_url}, headers={'Authorization': test_auth_header})
+        response = client.post('/', json={'url': test_url}, headers={'Authorization': test_auth_header_a})
         test_key_exists = response.json['data']['id']
 
+    def test_modify_other_user_should_fail(self, client):
+        response = client.put(f'/{test_key_exists}', json={'url': test_url}, headers={'Authorization': test_auth_header_b})
+        assert response.status_code == 403
+        assert response.json['status'] == 'error'
+        assert response.json['data'] == { 'message': 'Forbidden' }
+        response = client.delete(f'/{test_key_exists}', headers={'Authorization': test_auth_header_b})
+        assert response.status_code == 403
+        assert response.json['status'] == 'error'
+        assert response.json['data'] == { 'message': 'Forbidden' }
+
     def test_get_keys_from_root_should_succeed(self, client):
-        response = client.get('/', headers={'Authorization': test_auth_header})
+        response = client.get('/', headers={'Authorization': test_auth_header_a})
         assert response.status_code == 200
         assert response.json['status'] == 'success'
         assert type(response.json['data']['shortened_urls']) == list
@@ -73,23 +84,17 @@ class TestInterface(TestBase):
         assert type(list_item['url']) == str
 
     def test_create_key_should_succeed(self, client):
-        response = client.post('/', json={'url': test_url}, headers={'Authorization': test_auth_header})
+        response = client.post('/', json={'url': test_url}, headers={'Authorization': test_auth_header_a})
         assert response.status_code == 201
         assert response.json['status'] == 'success'
         assert type(response.json['data']['id']) == str
 
     def test_create_key_with_invalid_url_should_fail(self, client):
-        response = client.post('/', json={'url': 'foo'}, headers={'Authorization': test_auth_header})
+        response = client.post('/', json={'url': 'foo'}, headers={'Authorization': test_auth_header_a})
         assert response.status_code == 400
         assert response.json['status'] == 'error'
         assert response.json['data'] == { 'message': 'Invalid URL' }
 
     def test_delete_root_should_fail(self, client):
-        response = client.delete('/', headers={'Authorization': test_auth_header})
+        response = client.delete('/', headers={'Authorization': test_auth_header_a})
         assert response.status_code == 404
-
-class TestLogic(TestBase):
-    # TODO Add more tests
-
-    def test_key_exists_after_creation(self, client):
-        raise NotImplementedError
